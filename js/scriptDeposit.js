059bccb888264a4a86be5b1ed8293b3c08e1998c
slimmzy <slimmzy>
2023-10-20 02:23:07 +0100

    addup

--- js/scriptDeposit.js
+++ js/scriptDeposit.js
@@ -1,31 +1,87 @@
-const copyDeposit = document.getElementById('copyDeposit');
-const depositAdress = document.getElementById('depositAddress');
 
-copyDeposit.addEventListener("click", copyDepositAddress);
 
-function copyDepositAddress() {
-	depositAddress.select();
-
-	document.execCommand("copy");
-
-	alert("Deposit Address copied to clipboard")
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-	const depositId = window.location.hash.substring(1);
-	const depositContainer = document.getElementById("depositId");
-
-	if (depositContainer) {
-		const depositAddressInput = depositContainer.querySelector("input");
-
-		rotateDepositAddress(depositAddressInput);
+const containers = document.querySelectorAll('.container');
+containers.forEach((container, index) => {
+	if (index === 0) {
+		container.style.display = 'block';
+	} else {
+		container.style.display = 'none';
 	}
 });
 
-function rotateDepositAddress(inputElement) {
-	const currentaddress = inputElement.value;
-	const nextConatiner = container.nextElementSibling || container.parentElement.firstElementChild;
-	const nextInput = nextConatiner.querySelector("input");
-	const nextAddress = nextInput.value;
-	nextInput.value = currentaddress;
-}
+//  function to toggle container visibility on page load or refresh
+window.addEventListener('load', () => {
+	const currentContainerIndex = Math.floor(Math.random() * containers.length);
+
+	containers.forEach((container, index) => {
+		if (index === currentContainerIndex) {
+
+			container.style.display = 'block';
+		} else {
+
+			container.style.display = 'none';
+		}
+	});
+});
+
+
+
+document.getElementById("copyDeposit1").addEventListener("click", function() {
+	var depositAddress = document.getElementById("depositAddress1");
+
+	navigator.clipboard.writeText(depositAddress.value).then(function() {
+		alert("Deposit Address coplied to clipboard: " + depositAddress.value);
+	})
+
+	.catch(function(error) {
+		console.error("Falied to copy deposit Address:", error);
+	});
+});
+
+document.getElementById("copyDeposit2").addEventListener("click", function() {
+	var depositAddress = document.getElementById("depositAddress2");
+
+	navigator.clipboard.writeText(depositAddress.value).then(function() {
+		alert("Deposit Address coplied to clipboard: " + depositAddress.value);
+	})
+
+	.catch(function(error) {
+		console.error("Falied to copy deposit Address:", error);
+	});
+});
+
+document.getElementById("copyDeposit3").addEventListener("click", function() {
+	var depositAddress = document.getElementById("depositAddress3");
+
+	navigator.clipboard.writeText(depositAddress.value).then(function() {
+		alert("Deposit Address coplied to clipboard: " + depositAddress.value);
+	})
+
+	.catch(function(error) {
+		console.error("Falied to copy deposit Address:", error);
+	});
+});
+
+document.getElementById("copyDeposit4").addEventListener("click", function() {
+	var depositAddress = document.getElementById("depositAddress4");
+
+	navigator.clipboard.writeText(depositAddress.value).then(function() {
+		alert("Deposit Address coplied to clipboard: " + depositAddress.value);
+	})
+
+	.catch(function(error) {
+		console.error("Falied to copy deposit Address:", error);
+	});
+});
+
+document.getElementById("copyDeposit5").addEventListener("click", function() {
+	var depositAddress = document.getElementById("depositAddress5");
+
+	navigator.clipboard.writeText(depositAddress.value).then(function() {
+		alert("Deposit Address coplied to clipboard: " + depositAddress.value);
+	})
+
+	.catch(function(error) {
+		console.error("Falied to copy deposit Address:", error);
+	});
+});
